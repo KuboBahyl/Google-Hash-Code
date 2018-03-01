bc4d8dbf5e0b195459ea1f466c4a58127c8c193a
Lejla Metohajrova <lejla.metohajrova@exponea.com>
2018-03-01 20:07:29 +0100

    .

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 # THE MAINEST FILE
 
 import time as t
+import numpy as np
 from tqdm import tqdm # loading bars
 
 from processing import preprocess, postprocess
 from InOut import *
 from best_solution import *
+
+import numpy as np
+
+def ride_length(ride):
+    xlen = np.absolute(ride[0] - ride[1])
+    ylen = np.absolute(ride[2] - ride[3])
+    return xlen + ylen
+
+def if_bonus(, ride):
+    pass
+
+def calc_score(cars, rides):
+    pass(ride):
+    """
+    calculates time needed for a ride
+    """
+    return np.absolute(ride[0] - ride[2]) + np.absolute(ride[1] - ride[3])
 
 def read_input(path):
     with open(path, 'r') as f:
@@ -16,13 +34,21 @@ def read_input(path):
         for ride in range(Nrides):
             rides.append([int(i) for i in f.readline().split(" ")])
 
+        sorted(rides, key = lambda x: x[4])
+
+    return {
+        'Nrows': Nrows,
+        'Ncols': Ncols,
+        'Ncars': Ncars,
+        'bonus': bonus,
+        'Nsteps': Nsteps,
+        'rides': rides
+    }
+
 def write_output(path, *args):
     with open(path, 'w') as f:
         f.write("best output")
         f.write('\n')
-
-def preprocess():
-    pass
 
 def postprocess():
     pass
@@ -49,6 +75,3 @@ if __name__ == '__main__':
 
     end_start = t.time()
     print("Preprocessing time: {0:.2f}s".format(end_start - time_start))
-
-
-main()
