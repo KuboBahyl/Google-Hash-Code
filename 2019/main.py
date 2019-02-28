b85494235e9ddb339a38271558fb0967a508f621
Marek Bruchatý <marekbruchaty@gmail.com>
2019-02-28 19:13:23 +0100

    Parser

--- 2019/main.py
+++ 2019/main.py
@@ -5,13 +5,43 @@ python3 main.py data/example.in out.txt
 
 import sys
 
+files = ["../data/a_example.txt",
+         "../data/b_lovely_landscapes.txt",
+         "../data/e_shiny_selfies.txt",
+         "../data/d_pet_pictures.txt",
+         "../data/c_memorable_moments.txt"]
+
+collection_len = 0
+photos = []
+
+class Photo:
+    def __init__(self, orientation, tag_count, tags):
+        self.orientation = orientation
+        self.tag_count = tag_count
+        self.tags = tags
+        self.is_vertical = orientation == 'V'
+
+    def __str__(self):
+        return "orientation: {}, tag_count: {}, tags: {}".format(self.orientation, self.tag_count, self.tags)
 
 def parse_input(file):
     nums = []
+    collection = []
     with open(file, 'r') as f:
         for i, line in enumerate(f):
             # TODO parsing
-            nums.append(line.rstrip('\n').split(' '))
+
+            if i == 0:
+                collection_len = int(line.strip())
+                continue
+
+            line_split = str(line).strip().split(' ')
+            orientation = line_split[0]
+            tag_count = line_split[1]
+            tags = line_split[2:]
+            photo = Photo(orientation=orientation, tag_count=tag_count, tags=tags)
+            print(photo)
+            photos.append(photo)
 
     return nums
 
@@ -25,5 +55,5 @@ def write_out(file, data):
 
 
 if __name__ == '__main__':
-    nums = parse_input(sys.argv[1])
+    nums = parse_input(files[0])
     write_out(sys.argv[2] if len(sys.argv) > 2 else 'out.txt', nums)
