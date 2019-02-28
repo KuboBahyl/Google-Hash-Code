49dfa9fbb091a70e6104276a5e8f5d600d1500ab
Jakub Bahyl <bahylkubo@gmail.com>
2019-02-28 21:58:09 +0100

    best main ever

--- 2019/main.py
+++ 2019/main.py
@@ -27,7 +27,7 @@ class Photo:
         self.is_vertical = orientation == 'V'
 
     def __str__(self):
-        return "orientation: {}, tag_count: {}, tags: {}".format(self.orientation, self.tag_count, self.tags)
+        return "id: {}, orientation: {}, tag_count: {}, tags: {}".format(self.id, self.orientation, self.tag_count, self.tags)
 
     # def __eq__(self, photo):
     #     return self.id == photo.id
@@ -64,25 +64,38 @@ def parse_input(file):
     return photos
 
 
-def write_out(file, slideshow_ver, slideshow_hor):
-    verticals = int(len(slideshow_ver) / 2)
-    slideshow_len = verticals + len(slideshow_hor)
-    slideshow = slideshow_ver + slideshow_hor
+def write_out(file, slideshow):
+    sli_len = 0
+    is_vert = False
+    for photo in slideshow:
+        if photo.is_vertical:
+            if is_vert:
+                is_vert = False
+                sli_len += 1
+            else:
+                is_vert = True
+        else:
+            sli_len += 1
+
 
     with open(file, 'w') as f:
-        f.write('{}\n'.format(slideshow_len))
+        f.write('{}\n'.format(sli_len))
 
+        is_vert = False
         for i, photo in enumerate(slideshow):
-            if i < len(slideshow) - 1:
-                if not photo.is_vertical:
-                    f.write('{}\n'.format(photo.id))
+            f.write('{}'.format(photo.id))
+            if photo.is_vertical:
+                if is_vert:
+                    f.write('\n'.format(photo.id))
+                    is_vert = False
                 else:
-                    if i%2 == 0:
-                        f.write('{} '.format(photo.id))
-                    else:
-                        f.write('{}\n'.format(photo.id))
+                    f.write(' '.format(photo.id))
+                    is_vert = True
             else:
-                f.write('{}'.format(photo.id))
+                if i == len(slideshow) - 1:
+                    continue
+                else:
+                    f.write('\n'.format(photo.id))
 
 
 def greedy(photos):
@@ -97,6 +110,9 @@ def greedy(photos):
                 best_score = score_temp
                 best_1 = photo1
                 best_2 = photo2
+    # best_1 = photos[0]
+    # best_2 = photos[1]
+    # best_score = find_min(best_1.tags, best_2.tags)
 
     slideshow = [best_1, best_2]
     score += best_score
@@ -105,49 +121,87 @@ def greedy(photos):
     best_right = 0
     best_left_photo = None
     best_right_photo = None
+
+    if slideshow[0].is_vertical:
+        is_left_vert = True
+    else:
+        is_left_vert = False
+
+    if slideshow[1].is_vertical:
+        is_right_vert = True
+    else:
+        is_right_vert = False
+
     used_inds = set([best_1.id, best_2.id])
 
     for i in tqdm(range(photos_len)):
+        # print('slideshow')
+        # print(is_left_vert)
+        # for photo in slideshow:
+        #     print(photo)
+
         for photo in photos:
             if photo.id not in used_inds:
 
                 # left
-                score_temp = find_min(photo.tags, slideshow[0].tags)
-                if score_temp > best_left:
-                    best_left = score_temp
-                    best_left_photo = photo
+                if (is_left_vert and photo.is_vertical) or not is_left_vert:
+                    score_temp = find_min(photo.tags, slideshow[0].tags)
+                    if score_temp > best_left:
+                        best_left = score_temp
+                        best_left_photo = photo
 
                 # right
-                score_temp = find_min(photo.tags, slideshow[-1].tags)
-                if score_temp > best_right:
-                    best_right = score_temp
-                    best_right_photo = photo
+                if (is_right_vert and photo.is_vertical) or not is_right_vert:
+                    score_temp = find_min(photo.tags, slideshow[-1].tags)
+                    if score_temp > best_right:
+                        best_right = score_temp
+                        best_right_photo = photo
 
         if best_right >= best_left and best_right_photo:
             slideshow.append(best_right_photo)
             score += best_right
             used_inds.add(best_right_photo.id)
+            if best_right_photo.is_vertical:
+                if is_right_vert:
+                    is_right_vert = False
+                else:
+                    is_right_vert = True
+
         elif best_left_photo:
             slideshow.insert(0, best_left_photo)
             score += best_left
             used_inds.add(best_left_photo.id)
+            if best_left_photo.is_vertical:
+                if is_left_vert:
+                    is_left_vert = False
+                else:
+                    is_left_vert = True
 
         best_left = 0
         best_right = 0
         best_left_photo = None
         best_right_photo = None
 
-    # for photo in slideshow:
-    #     print(photo)
+
+    if is_left_vert:
+        slideshow = slideshow[1:]
+    if is_right_vert:
+        slideshow = slideshow[:-1]
 
     return slideshow, score
 
 if __name__ == '__main__':
-    photos = parse_input(files[3])
-    horizontal = [photo for photo in photos if not photo.is_vertical]
-    slideshow_hor, score = greedy(horizontal)
+    photos = parse_input(files[2])
+    # horizontal = [photo for photo in photos if not photo.is_vertical]
+    #
+    # slideshow_hor, score = greedy(horizontal)
+    #
+    # vertical = [photo for photo in photos if photo.is_vertical]
+    # slideshow_ver, score = greedy(vertical)
 
-    vertical = [photo for photo in photos if photo.is_vertical]
-    slideshow_ver, score = greedy(vertical)
+    slideshow, score = greedy(photos)
+    for photo in slideshow:
+        print(photo)
 
-    write_out(sys.argv[1], slideshow_ver, slideshow_hor)
+    print(score)
+    write_out(sys.argv[1], slideshow)
