bea62e3b7516553a8300b891f3f23c1956e09796
ondrej.brichta <ondrej.brichta@gmail.com>
2020-02-20 19:10:07 +0100

    load data

--- 2020/helpers.py
+++ 2020/helpers.py
@@ -1,10 +1,16 @@
 def parse_input(path):
     with open(path, 'r') as f:
         lines = f.readlines()
-        n_rows = lines[0]
-
-        collection = []
-        for line in lines[1:]:
-            val_1, val_2 = line.strip().split(' ')
-            # TODO
-    return photos
+        n_books, n_libraries, days = lines[0].strip().split(' ')
+        book_scores = lines[1].strip().split(' ')
+        libraries = []
+        for i in range(int(n_libraries)):
+             n_b, p_l, b_p_d = lines[2+i*2].strip().split(' ')
+             books = lines[2+i*2].strip().split(' ')
+             libraries.append({
+                 'n_b':n_b,
+                 'p_l':p_l,
+                 'b_p_d':b_p_d,
+                 'books':books
+             })
+    return n_books,n_libraries, days ,book_scores, libraries
